86f1a5e23b96243923b308ee63ed3382afa73572
Ygor Kayan <ygor.freitas@vinci-energies.net>
2024-10-01 09:42:34 +0100

    fix: um problema de compatibilidade entre jest e cypress

--- src/app/components/card/close/close.component.ts
+++ src/app/components/card/close/close.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "close-svg",
   standalone: true,
-  templateUrl: "./close.component.svg",
+  templateUrl: "./close.component.html",
   styleUrls: ["./close.component.css"],
 })
 export class Close {}
