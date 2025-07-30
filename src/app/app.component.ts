9215e151beb4ead0198a6621ec3fa4ddd94c65a0
Ygor Kayan <ygor.araujo.freitas@winning.pt>
2025-07-30 11:06:38 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
   constructor(private http?: HttpClient) {}
 
   title = "Teste de Desenvolvedor Front-End - Anota Ai";
-  subtitle = "Teste branch winning-2";
+  subtitle = "Teste branch winning-2-2";
   cards: Card[] = [];
 
   ngOnInit() {
